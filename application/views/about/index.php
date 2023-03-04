963214324088670883aacef343af9e62e0d1069b
Jahidinn <jahidin.inspirit@gmail.com>
2023-03-05 01:07:21 +0700

    Upload File CIAPP

---
+++ application/views/about/index.php
@@ -0,0 +1,17 @@
+<div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">About CI APP</h1>
+      <p class="lead">Dibuat oleh jahidin 2019</p>
+      <hr class="my-4">
+        <p class="lead">Layanan ini merupakan contoh layanan sederhana manajemen data mahasiswa (fitur dasar) menggunakan Codeigniter(CI) yang bisa di kembangkan untuk membuat sebuah sistem lanjutan</p>
+        <ul>
+        	<li>Proses pembuatan dilakukan dengan Codeigniter, database MySQL, bootstrap fraemwork</li>
+        	<li>Menampilkan data mahasiswa</li>
+        	<li>Input data mahasiswa</li>
+        	<li>Hapus data mahasiswa</li>
+        	<li>Menampilkan detail data mahasiswa</li>
+        	<li>Fitur Pencarian Data</li>
+        </ul>
+    </div>	
+</div>
+
---
+++ application/views/home/index.php
@@ -0,0 +1,11 @@
+
+<div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">Hello, Selamat Datang!</h1>
+      <p class="lead">Layanan Manajemen Data Mahasiswa (Codeigniter)</p>
+      <hr class="my-4">
+        <p>Layanan ini merupakan contoh layanan sederhana manajemen data mahasiswa yang dibuat menggunakan <span class="font-weight-bold">Codeiniter</span> yang bisa digunakan untuk membuat sistem manajemen informasi lanjutan</p>
+        <a class="btn btn-primary btn-lg" href="<?= base_url(); ?>mahasiswa" role="button">Lihat data</a>
+    </div>	
+</div>
+
